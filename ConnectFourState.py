f3109d90938277785751cf7e16e8b955843ee803
Naris <nariscatboy@gmail.com>
2018-04-12 04:10:40 +1000

    cached calculations

--- ConnectFourState.py
+++ ConnectFourState.py
@@ -12,6 +12,9 @@ class ConnectFour:
             self.state = np.zeros((self.width, self.height), dtype=int)
         self.turn = turn
         self.winner = None
+        self.hori = [None]*4
+        self.vert = [None]*4
+        self.diag = []
 
     @classmethod
     def from_string(cls, turn, state):
@@ -51,29 +54,43 @@ class ConnectFour:
         else:
             return False
 
+    def consec_hor(self, n):
+        if self.hori[n] is None:
+            row_summer = np.triu(np.tril(np.ones((self.width, self.width), dtype=int)), 1 - n)
+            self.hor[n] = (self.state @ row_summer)[:, :1-n]
+        return self.hor[n]
+        
+    def consec_ver(self, n):
+        if self.vert[n] is None:
+        col_summer = np.tril(np.triu(np.ones((self.height, self.height), dtype=int)), n - 1)
+        return (col_summer @ self.state)[:1-n, :]
+
+    def consec_diag(self, n):
+        if not self.diag:
+            self.diag = [None]*4
+            mdiag, mshift, odiag, oshift = [np.copy(self.state) for _ in range(4)]
+            up_shift = np.eye(self.height, k=1, dtype=int)
+            left_shift = np.eye(self.width, k=-1, dtype=int)
+
+            for i in range(1, 4):
+                mshift = up_shift @ mshift @ left_shift
+                oshift = up_shift @ oshift @ left_shift.T
+                mdiag += mshift
+                odiag += oshift
+                self.diag[i] = np.copy(mdiag[:1-i, :1-i]), np.copy(odiag[:1-i, i:])
+        return self.diag[n]
+
     def count_consec(self, n, player):
-        score = n*player
         if n == 1:
             return np.count_nonzero(self.state == player)
 
-        row_summer = np.triu(np.tril(np.ones((self.width, self.width), dtype=int)), 1 - n)
-        col_summer = np.tril(np.triu(np.ones((self.height, self.height), dtype=int)), n - 1)
+        score = n*player
+        hori = np.count_nonzero(self.consec_hor == score)
+        vert = np.count_nonzero(self.consec_ver == score)
 
-        hori = np.count_nonzero((self.state @ row_summer)[:, :1-n] == score)
-        vert = np.count_nonzero((col_summer @ self.state)[:1-n, :] == score)
-
-        mdiag, mshift, odiag, oshift = [np.copy(self.state) for _ in range(4)]
-        up_shift = np.eye(self.height, k=1, dtype=int)
-        left_shift = np.eye(self.width, k=-1, dtype=int)
-
-        for i in range(1, n):
-            mshift = up_shift @ mshift @ left_shift
-            oshift = up_shift @ oshift @ left_shift.T
-            mdiag += mshift
-            odiag += oshift
-
-        mdiag = np.count_nonzero(mdiag[:1-n, :1-n] == score)
-        odiag = np.count_nonzero(odiag[:1-n, n:] == score)
+        mdiag, odiag = self.consec_diag(n)
+        mdiag = np.count_nonzero(mdiag == score)
+        odiag = np.count_nonzero(odiag == score)
 
         return hori + vert + mdiag + odiag
         
