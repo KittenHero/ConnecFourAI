9233ee2b4e795f3151b998c63b1d4eef7cb94ba7
Naris <nariscatboy@gmail.com>
2018-04-12 03:55:28 +1000

    ab tiebreak

--- Agents.py
+++ Agents.py
@@ -37,39 +37,39 @@ class AlphaBetaAgent:
 
     def compute_action(self, state):
         self.expanded = 0
-        _, _, action = self.alphabeta(state)
+        _, action = self.alphabeta(state)
         return action
 
     def alphabeta(self, state, alpha=float('-inf'), beta=float('inf'), depth=0, action=None):
         self.expanded += 1
         actions = state.legal_actions[1:] # discard skip
         if not actions or depth == self.depth:
-            return self.evalFn(state), self.expanded, action
+            return self.evalFn(state), action
         elif depth % 2:
             return min(self.beta(state, alpha, beta, depth, actions))
         else:
-            return max(self.alpha(state, alpha, beta, depth, actions))
+            return max(self.alpha(state, alpha, beta, depth, actions), key=lambda k: k[0])
 
     def beta(self, state, alpha, beta, depth, actions):
         for action in actions:
-            score, _, _ = self.alphabeta(
+            score, _ = self.alphabeta(
                 state.successor(action),
                 alpha, beta, depth + 1,
                 action
             )
-            yield score, action, action
+            yield score, action
             beta = min(beta, score)
             if beta <= alpha:
                 break
 
     def alpha(self, state, alpha, beta, depth, actions):
         for action in actions:
-            score, _, _ = self.alphabeta(
+            score, _ = self.alphabeta(
                 state.successor(action),
                 alpha, beta, depth + 1,
                 action
             )
-            yield score, state.width - action, action
+            yield score, action
             alpha = max(alpha, score)
             if beta <= alpha:
                 break
